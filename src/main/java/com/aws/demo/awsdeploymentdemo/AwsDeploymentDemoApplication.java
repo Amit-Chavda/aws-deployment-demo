e132628f0c82d2625aa59a25a01b49c059b7f666
Amit Chavda <amitchavda9879@gmail.com>
2022-08-30 08:46:45 +0530

    Initial commit

---
+++ src/main/java/com/aws/demo/awsdeploymentdemo/AwsDeploymentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.demo.awsdeploymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploymentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploymentDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/demo/awsdeploymentdemo/AwsDeploymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.demo.awsdeploymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeploymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

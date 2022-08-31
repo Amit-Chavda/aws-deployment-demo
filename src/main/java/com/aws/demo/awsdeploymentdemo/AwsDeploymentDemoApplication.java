259e2d8088aff925415fb8542a11b2cb416b4e1d
Amit Chavda <amitchavda9879@gmail.com>
2022-08-31 11:23:28 +0530

    Added: Sample API and chnage server port

--- src/main/java/com/aws/demo/awsdeploymentdemo/AwsDeploymentDemoApplication.java
+++ src/main/java/com/aws/demo/awsdeploymentdemo/AwsDeploymentDemoApplication.java
@@ -2,12 +2,20 @@ package com.aws.demo.awsdeploymentdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AwsDeploymentDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsDeploymentDemoApplication.class, args);
-	}
+    @GetMapping
+    public String welcome() {
+        return "Hello, this is demo for AWS deployment";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDeploymentDemoApplication.class, args);
+    }
 
 }
